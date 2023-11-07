cbb92e05ac4444024576e227d88f144efb435dd8
rafael.rehfeld <rafael.rehfeld@codificar.com.br>
2023-11-07 14:17:47 -0300

    fix.665: alterando a váriavel currentLocale para settings.language

--- lang/strings.js
+++ lang/strings.js
@@ -1,15 +1,13 @@
 import {NativeModules, Platform} from 'react-native';
 
-let strings = '';
+let strings = require('./pt_BR.json');
 
 const deviceLanguage =
 	Platform.OS === 'ios'
 		? NativeModules.SettingsManager.settings.AppleLanguages[0]
 		: NativeModules.I18nManager.localeIdentifier;
 
-  if(deviceLanguage === 'pt_BR'){
-    strings = require('./pt_BR.json');
-  } else if(deviceLanguage == 'es_PY' || 'es-PY'){
+  if(deviceLanguage == 'es_PY' || deviceLanguage.includes() == 'es'){
     strings = require('./es-PY.json');
   }
 
